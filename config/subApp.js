3a2da7ed57b55780c749c2ed55f4a8ca4029c955
cxy <itmanyong@gmail.com>
2022-10-22 11:39:43 +0800

    修复entry.js文件缺失

--- config/subApp.js
+++ config/subApp.js
@@ -1,9 +1,9 @@
 /*
- * @FilePath: \hel-micro-utils\config\subApp.js
+ * @FilePath: \hel-micro-lodash\config\subApp.js
  * @Date: 2022-09-24 03:03:23
  * @Author: itmanyong@gmail.com
  * @LastEditors: itmanyong@gmail.com
- * @LastEditTime: 2022-09-24 04:16:07
+ * @LastEditTime: 2022-09-26 21:26:16
  * @Description:
  * @unpkg链接：https://unpkg.com/hel-micro-lodash@latest/hel_dist/hel-meta.json
  */
@@ -12,5 +12,4 @@ const helDevUtils = require('hel-dev-utils');
 const pkg = require('../package.json');
 
 const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg' });
-
 module.exports = subApp;
